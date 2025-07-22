4c93616a58dd74f3a1b52370d846c5383c15e1e2
Fatima Rahimi <rahimifatima432@gmail.com>
2025-07-22 17:23:16 +0430

    create a todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import { useState } from "react";
+
 function App() {
+  // state hook - 'useState'
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+  
+  // Helper function
+
+  function addItem(){
+    if(!newItem){
+      alert("Enter an item.");
+      return;
+    };
+
+    const item = {
+      id: Math.floor(Math.random() * 1000),
+      value: newItem
+    };
+
+    setItems(oldList => [...oldList, item]);
+    setNewItem("");
+  };
+
+   function deleteItem(id) {
+     const newArray = items.filter(item => item.id !== id);
+     setItems(newArray);
+   }
   return(
-     <h1 className="text-3xl font-bold text-red-500 text-center">Todo List</h1>
+    <div className="text-center">
+     {/* 1.Header */}
+      <h1 className="font-bold text-4xl p-5">Todo List App</h1>
+      {/* 2.input (input and button) */}
+      <input className="p-[4px] mr-[8px] outline-none border-2 rounded-md border-gray-400 " type="text"  placeholder="Add and item..." value={newItem}
+      onChange={e => setNewItem(e.target.value)}/>
+      <button className="bg-gray-100 px-2 border-2 border-gray-400 rounded-md text-sm font-semibold py-1" onClick={() => addItem()}>Add</button>
+      {/* 3.list of items (unordred list with list items) */}
+     <ul className="list-none">
+      {items.map(item => {
+        return(
+          <li className="mt-[12px] mr-[0] mb-[12px] ml-[0]"  key={item.id}>{item.value} <button className="text-[13px] px-1 ml-[8px] border-none text-white rounded-md pt-[4px]
+          pb-[4px] bg-gray-200 " onClick={() => deleteItem(item.id)}>❌</button></li>
+        )
+      })}
+    </ul>
+ </div>
+
   )
-}
+ }
 
 export default App;
